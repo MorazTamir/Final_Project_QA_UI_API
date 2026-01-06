cdd5db3979c213aa2aee6da5652b07396e267e91
MorazTamir <morazt2@gmail.com>
2026-01-06 21:30:51 +0200

    Add logic and config structure and initial files

--- infra/browser_wrapper.py
+++ infra/browser_wrapper.py
@@ -1,26 +1,26 @@
 from selenium import webdriver
-from selenium.common import WebDriverException
+from selenium.common.exceptions import WebDriverException
+from config.config_provider import ConfigProvider
 
-#customer@practicesoftwaretesting.com Welcome10!
 class BrowserWrapperClass:
-
     def __init__(self):
         print('Test Start!')
         self.driver = None
-        # self.config =
+        self.config = ConfigProvider()
 
-    def get_driver(self, browser, url):
+    def get_driver(self):
+        browser = self.config.get_browser()
+        url = self.config.get_base_url()
         try:
-            if browser == 'Chrome':
+            if browser.lower() == 'chrome':
                 self.driver = webdriver.Chrome()
-            elif browser == 'FireFox':
+            elif browser.lower() == 'firefox':
                 self.driver = webdriver.Firefox()
-
             self.driver.get(url)
             self.driver.maximize_window()
             return self.driver
-        except WebDriverException:
-            print("WebDriver Exception: Error while opening browser")
+        except WebDriverException as e:
+            print(f"WebDriver Exception: {e}")
 
     def close_browser(self):
         print("Test finish")
