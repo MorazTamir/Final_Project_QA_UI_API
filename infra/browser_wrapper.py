4ee0fcc0dec71297772ea03306f41852aac888f7
MorazTamir <morazt2@gmail.com>
2026-01-06 17:45:35 +0200

    Initial commit - Readme File and Infra

---
+++ infra/browser_wrapper.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.common import WebDriverException
+
+#customer@practicesoftwaretesting.com Welcome10!
+class BrowserWrapperClass:
+
+    def __init__(self):
+        print('Test Start!')
+        self.driver = None
+        # self.config =
+
+    def get_driver(self, browser, url):
+        try:
+            if browser == 'Chrome':
+                self.driver = webdriver.Chrome()
+            elif browser == 'FireFox':
+                self.driver = webdriver.Firefox()
+
+            self.driver.get(url)
+            self.driver.maximize_window()
+            return self.driver
+        except WebDriverException:
+            print("WebDriver Exception: Error while opening browser")
+
+    def close_browser(self):
+        print("Test finish")
+        if self.driver:
+            self.driver.quit()
